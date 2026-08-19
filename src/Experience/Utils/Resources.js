3160ed167c23301baf60d31e427647fe81f81a2f
Cell <marcell.made@gmail.com>
2026-08-19 14:06:57 -0400

    new scene added

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -1,81 +1,61 @@
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 import EventEmitter from "./EventEmitter";
- 
-export default class Resources extends EventEmitter
- {
-    constructor(sources)
-    {
-        super()
 
-        // Options
-        this.sources = sources
+export default class Resources extends EventEmitter {
+  constructor(sources) {
+    super();
 
-        // Setup
-        this.items = {}
-        this.toLoad = this.sources.length
-        this.loaded = 0
+    // Options
+    this.sources = sources;
 
-        this.setLoaders()
-        this.startLoading()
+    // Setup
+    this.items = {};
+    this.toLoad = this.sources.length;
+    this.loaded = 0;
+
+    this.setLoaders();
+    // this.startLoading()
+    this.loaders.gltfLoader.load(sources[4].path, (file) => {
+      console.log("loaded", file);
+    });
+    console.log(sources[4].path);
+  }
+
+  setLoaders() {
+    this.loaders = {};
+    this.loaders.gltfLoader = new GLTFLoader();
+    this.loaders.textureLoader = new THREE.TextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+  }
+
+  startLoading() {
+    // Load each source
+    for (const source of this.sources) {
+      if (source.type === "gltfModel") {
+        this.loaders.gltfLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "texture") {
+        this.loaders.textureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      } else if (source.type === "cubeTexture") {
+        this.loaders.cubeTextureLoader.load(source.path, (file) => {
+          this.sourceLoaded(source, file);
+        });
+      }
     }
+  }
 
-    setLoaders()
-    {
-        this.loaders = {}
-        this.loaders.gltfLoader = new GLTFLoader()
-        this.loaders.textureLoader = new THREE.TextureLoader()
-        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
+  sourceLoaded(source, file) {
+    this.items[source.name] = file;
+
+    this.loaded++;
+
+    if (this.loaded === this.toLoad) {
+      this.trigger("ready");
     }
-
-    startLoading()
-    {
-        // Load each source
-        for(const source of this.sources)
-        {
-            if(source.type === 'gltfModel')
-            {
-                this.loaders.gltfLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-            else if(source.type === 'texture')
-            {
-                this.loaders.textureLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-            else if(source.type === 'cubeTexture')
-            {
-                this.loaders.cubeTextureLoader.load(
-                    source.path,
-                    (file) =>
-                    {
-                        this.sourceLoaded(source, file)
-                    }
-                )
-            }
-        }
-    }
-
-    sourceLoaded(source, file)
-    {
-        this.items[source.name] = file
-
-        this.loaded++
-
-        if(this.loaded === this.toLoad)
-        {
-            this.trigger('ready')
-        }
-    }
- }
+  }
+}
--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,31 +1,34 @@
 export default [
-    {
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-            [
-                'textures/environmentMap/px.jpg',
-                'textures/environmentMap/nx.jpg',
-                'textures/environmentMap/py.jpg',
-                'textures/environmentMap/ny.jpg',
-                'textures/environmentMap/pz.jpg',
-                'textures/environmentMap/nz.jpg'
-            ]
-    },
-    {
-        name: 'grassColorTexture',
-        type: 'texture',
-        path: 'textures/dirt/color.jpg'
-    },
-    {
-        name: 'grassNormalTexture',
-        type: 'texture',
-        path: 'textures/dirt/normal.jpg'
-    },
-    {
-        name: 'foxModel',
-        type: 'gltfModel',
-        path: 'models/Fox/glTF/Fox.gltf'
-    }
-
-]
+  {
+    name: "environmentMapTexture",
+    type: "cubeTexture",
+    path: [
+      "textures/environmentMap/px.jpg",
+      "textures/environmentMap/nx.jpg",
+      "textures/environmentMap/py.jpg",
+      "textures/environmentMap/ny.jpg",
+      "textures/environmentMap/pz.jpg",
+      "textures/environmentMap/nz.jpg",
+    ],
+  },
+  {
+    name: "grassColorTexture",
+    type: "texture",
+    path: "textures/dirt/color.jpg",
+  },
+  {
+    name: "grassNormalTexture",
+    type: "texture",
+    path: "textures/dirt/normal.jpg",
+  },
+  {
+    name: "foxModel",
+    type: "gltfModel",
+    path: "models/Fox/glTF/Fox.gltf",
+  },
+  {
+    name: "world-burger-baked",
+    type: "glbModel",
+    path: "models/world-burger-baked.glb",
+  },
+];
